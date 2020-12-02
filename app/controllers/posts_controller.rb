854d9cafe5f205a4f6f6187a13e769db93e30925
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2020-12-02 12:03:25 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,12 @@
 class PostsController < ApplicationController
   #indexアクションを定義　リソース一覧を表示する
   def index
-    @posts = Post.all
-  end
-
-  def new
+    @posts = Post.all.order(id: "DESC")
   end
 
   def create
      Post.create(content: params[:content])
+     redirect_to action: :index
   end
 
 end
